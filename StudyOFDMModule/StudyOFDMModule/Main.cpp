a0149c67f93117fb42eb7c59f636c5d7cdc414fb
aluminum88 <hogehoge@gmail.com>
2019-03-07 16:50:37 +0900

    add
    デバックファイルの無視

--- StudyOFDMModule/StudyOFDMModule/Main.cpp
+++ StudyOFDMModule/StudyOFDMModule/Main.cpp
@@ -5,18 +5,20 @@ void Main()
 {
 	Graphics::SetBackground(ColorF(0.8, 0.9, 1.0));
 
-	const Font font(60);
+	const Font font(30);
 
 	const Texture textureCat(Emoji(U"🐈"), TextureDesc::Mipped);
 
+	int setnum = 0;
+
+
+
+
 	while (System::Update())
 	{
-		font(U"Hello, Siv3D!🐣").drawAt(Window::Center(), Palette::Black);
+		
+		font(Cursor::Pos()).draw(20, 550, ColorF(0.6));
 
-		font(Cursor::Pos()).draw(20, 500, ColorF(0.6));
-
-		textureCat.resized(80).draw(700, 500);
-
-		Circle(Cursor::Pos(), 60).draw(ColorF(1, 0, 0, 0.5));
+		
 	}
 }
